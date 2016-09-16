1f9d1b54294137796aab5f41320229aad96b3690
zhuodong45 <zhuodong45@gmail.com>
2016-09-16 00:30:01 -0400

    fixed typo

--- build/js/mytyped.js
+++ build/js/mytyped.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
   //typed.js
   $("#typed").typed({
-    strings: ["Hello Visitors.","Hello World.","Wellcome to my website."],
+    strings: ["Hello Visitors.","Hello World.","Welcome to my website."],
     backDelay: 700,
     typeSpeed: 50
   });
