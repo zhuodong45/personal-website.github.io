f1065b6571a7514edddf499c2964dfdeb69c0390
zhuodong45 <zhuodong45@gmail.com>
2016-09-25 00:30:47 -0400

    new about-me

--- app/component/aboutMe.js
+++ app/component/aboutMe.js
@@ -7,26 +7,44 @@ export default class AboutMe extends React.Component {
   }
   render() {
     return (
-    <div className="container div-size">
-      <div>
-        <div className="title-style put-mid"><font>ABOUT ME</font></div>
-        <hr className="hr-style"/>
-        <div className="put-mid"><img className="put-mid" src="./build/img/IMG_2943_meitu_1.jpg" width="70%" /></div>
-
-        <div id="aboutme-not-type" className="mypanel">
-          <font className="aboutme-font-style">
-            My name is Zhuodong Huang, a programmer in this world. I am a student in UMass Amherst. Computer science is my major. I love programming and I enjoy the time when I work on my projects.
-            <br/>
-            <br/>· Experiences: Java, C, HTML/CSS, JavaScript, SQL, Node.js, MongoDB, Scala, Android, Python.
-            <br/>· Platforms: Windows, Linux.
-            <br/>
-            <br/>· Email: zhuodong45@gmail.com
-            <br/>
-            <br/>For more detail, you can take a look at my Resume or <a className="font-color" href="mailto:zhuodong45@gmail.com">email me</a>.
-          </font>
+      <div className="container">
+        <div>
+          <center><div className="title-style"><font>About Me</font></div></center>
+          <hr className="hr-style"/>
+          <div className="row">
+            <div className="col-md-4 col-md-offset-1">
+              <img src="./build/img/aboutme-1.jpg" width="100%"/>
+            </div>
+            <div className="col-md-6">
+              <br/>
+              <font className="aboutme-font-style">My name is Zhuodong Huang, a software engineer in this world. I am a student in UMass Amherst. Computer science is my major. I love programming and I enjoy the time when I work on my projects.</font>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-md-6 col-md-offset-1">
+              <br/>
+              <font className="aboutme-font-style">· Programming languages: Java, C, HTML/CSS, JavaScript, Scala, Android, Python.
+              <br/>· Platforms: Windows, Linux.
+              <br/>· Tools: Eclipse, IntelliJ IDEA, Android Studio, PyCharm, VirtualBox</font >
+            </div>
+            <div className="col-md-4">
+              <img src="./build/img/aboutme-2.jpg" width="100%"/>
+            </div>
+          </div>
+          <br/>
+          <div className="row">
+            <div className="col-md-4 col-md-offset-1">
+              <img src="./build/img/aboutme-3.jpg" width="100%"/>
+            </div>
+            <div className="col-md-6">
+              <br/>
+              <font className="aboutme-font-style">For more detail, you can take a look at my Resume or <a className="font-color" href="mailto:zhuodong45@gmail.com">email me</a>.</font>
+            </div>
+          </div>
+          <br/>
         </div>
       </div>
-    </div>
     )
   }
 }
--- app/component/intro.js
+++ app/component/intro.js
@@ -10,7 +10,7 @@ export default class Intro extends React.Component {
       <span id="typed" className="typed-font-style"></span>
       <hr className="intro-hr-style"/>
       <div>
-        <font className="intro-font-style">“Without leaps of imagination, or dreaming, we lose the excitement of possibilities. Dreaming, after all, is a form of planning.”<br/>–– Gloria Steinem, Feminist<br/><br/>Here is A programmer and his dream.</font>
+        <font className="intro-font-style">“Without leaps of imagination, or dreaming, we lose the excitement of possibilities. Dreaming, after all, is a form of planning.”<br/>–– Gloria Steinem, Feminist<br/><br/>Here is a software engineer and his dream.</font>
       </div>
     </div>
     )
